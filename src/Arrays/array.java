71d15d951a7b19f5c216436835c9d86bce9830cb
rabbishakir <rabbishakir@gmail.com>
2023-01-26 09:18:14 -0500

    Array and List

---
+++ src/Arrays/array.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class array {
+
+    public void sendText(int[] to, String message){
+        int i;
+        for (i=0; i<5; i++){
+            // System.out.println(i);
+            System.out.println("Sending text to:" + to[i] + ". Message: " + message );
+        }
+
+
+    }
+}
